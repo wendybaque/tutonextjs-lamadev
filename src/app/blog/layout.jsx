c94c7cf03e35555ac2893f8bf91e303582997803
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-28 11:17:51 +0200

    Add Navbar & Footer components

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>BLOG LAYOUT</h1>
+      {children}
+    </div>
+  );
+};
+
+export default BlogLayout;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 - Made with ❤ by Wendy Baqué</div>
+      <div>IMAGES</div>
+    </div>
+  );
+};
+
+export default Footer;
