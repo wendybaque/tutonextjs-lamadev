cac8a1b14f301d381820aa5409f45085143e1e71
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-02 11:09:40 +0200

    Add Images in Next.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,20 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-   <div>HOME</div>
-  )
+    <div>
+      <div className={styles.texts}></div>
+      <div className={styles.imgContainer}>
+        {" "}
+        <Image
+          className={styles.img}
+          src="/hero.png"
+          alt="hero"
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>©2023 - Made with ❤ by Wendy Baqué</div>
-      <div>IMAGES</div>
+      <div>©2023 - Made with ❤ by <Link href="http://www.wendev.fr"alt="Redirect to WenDev website">Wendy Baqué</Link></div>
+      <div className={styles.social}>
+          <Link href="https://linktr.ee/wendev" alt="Facebook redirect"><Image src="/1.png" width={15} height={15} alt="Facebook" className={styles.icon}/></Link>
+          <Link href="https://linktr.ee/wendev" alt="Instagram redirect"><Image src="/2.png" width={15} height={15} alt="Instagram" className={styles.icon}/></Link>
+          <Link href="https://linktr.ee/wendev" alt="Twitter redirect"><Image src="/3.png" width={15} height={15} alt="Twitter" className={styles.icon}/></Link>
+          <Link href="https://linktr.ee/wendev" alt="Youtube redirect"><Image src="/4.png" width={15} height={15} alt="Youtube" className={styles.icon}/></Link>
+      </div>
     </div>
   );
 };
