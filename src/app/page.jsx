a775c131cdfc461f495fb51750e299958aede96c
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-02 11:25:52 +0200

    Stylize the hero part

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,17 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.texts}></div>
-      <div className={styles.imgContainer}>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>BETTER DESIGN FOR YOUR DIGITAL PRODUCTS</h1>
+        <p className={styles.desc}>
+          {" "}
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See our works</button>
+      </div>
+      <div className={styles.item}>
         {" "}
         <Image
           className={styles.img}
