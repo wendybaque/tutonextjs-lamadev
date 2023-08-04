e9a72349c8022d88ec69695c31b0ec3257588290
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-04 11:49:35 +0200

    Update portfolio page & style

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
-const Category = () => {
+const Category = ({ params }) => {
+  console.log(params);
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.containter}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      {/* {data.map((item) => ( */}
+        {/* <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} fill={true} src={item.image} alt="" />
+          </div>
+        </div> */}
+      {/* ))} */}
+    </div>
+  );
+};
 
-export default Category
+export default Category;
