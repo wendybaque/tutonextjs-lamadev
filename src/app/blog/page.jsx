25997817aed278e2446498cfccc21e1033999e54
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-04 11:55:43 +0200

    Update Blog page and posts with style

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import styles from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+    {/* {data.map((item) => (
+      <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={item.img}
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.desc}>{item.desc}</p>
+        </div>
+      </Link>
+    ))} */}
+  </div>
   )
 }
 
