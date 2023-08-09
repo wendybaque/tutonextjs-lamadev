0a94405a167c6eed95b5ee24e05fb2e2a4daa6cb
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-09 10:17:34 +0200

    Add context for dark mode

--- src/components/darkmodetoggle/DarkModeToggle.jsx
+++ src/components/darkmodetoggle/DarkModeToggle.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import styles from "./page.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const DarkModeToggle = () => {
-
-    const mode = "dark"
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
-      <div className={styles.icon}>🌙</div>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>☀️</div>
-      <div className={styles.ball} style={mode === "light" ? {left:"2px"}:{right:"2px"}}/>
+      <div className={styles.icon}>🌙</div>
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
     </div>
   );
 };
