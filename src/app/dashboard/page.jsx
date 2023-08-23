a4fe1b0da984af950d7daca6668a2a770787dbc4
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-08-23 11:01:22 +0200

    Use SWR for fetching data in dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,33 +1,46 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import useSWR from "swr";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [err, setErr] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
+  //OLD WAY TO FETCH DATA
 
-      if (!res.ok) {
-        setErr(true);
-      }
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-      const data = await res.json()
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
 
-      setData(data);
-      setIsLoading(false);
-    };
-    getData()
-  }, []);
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
 
-  console.log();
-  return <div className={styles.container}>Dashboard</div>;
+  //     const data = await res.json()
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData()
+  // }, []);
+
+  // const session = useSession();
+
+  // const router = useRouter();
+  
+  //NEW WAY TO FETCH DATA
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const {data, error, isLoading} = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
+ console.log(data)
 };
 
 export default Dashboard;
